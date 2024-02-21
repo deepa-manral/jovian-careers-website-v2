7b21a015aa11bb4b4cc7b5806925c0ff01287746
deepa-manral <157979028+deepa-manral@users.noreply.github.com>
2024-02-21 18:12:43 +0000

    final product

--- database.py
+++ database.py
@@ -26,3 +26,22 @@ def load_job_from_db(id):
       return None
     else:
       return rows[0]._asdict()
+
+
+def add_application_to_db(job_id, data):
+  with engine.connect() as conn:
+    query = text(
+        "INSERT INTO applications (job_id, full_name, email, linkedin, education,"
+        " work_experience, resume_) "
+        "VALUES (:job_id, :full_name, :email, :linkedin, "
+        ":education, :work_experience, :resume_)")
+    conn.execute(
+        query, {
+            "job_id": job_id,
+            "full_name": data['full_name'],
+            "email": data['email'],
+            "linkedin": data['linkedin'],
+            "education": data['edu'],
+            "work_experience": data['Workexp'],
+            "resume_": data['resume']
+        })
